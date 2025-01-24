0ba7d922ad57b90f2538be738096d42650febe01
A S <as@enoMacBook-Air.local>
2025-01-24 16:38:56 +0900

    2D仕様８割完成

--- 2DShooting/ShootingGame.java
+++ 2DShooting/ShootingGame.java
@@ -43,10 +43,14 @@ class Enemy {
         int enemyCenterX = x + size / 2;
         int enemyCenterY = y + size / 2;
 
-        if (playerCenterX < enemyCenterX) {
-            x -= 1;
-        } else {
-            x += 1;
+        int dy = player.y - y;
+
+        if(dy < 200) {
+            if (playerCenterX < enemyCenterX) {
+                x -= 1;
+            } else {
+                x += 1;
+            }
         }
         if (playerCenterY < enemyCenterY) {
             y -= 1;
@@ -72,10 +76,12 @@ class Player {
 
 class Fellow {
     protected int x, y, size = 20;
+    protected int difference;
 
-    public Fellow(int x, int y) {
+    public Fellow(int x, int y, Player player) {
         this.x = x;
         this.y = y;
+        difference = (int)Math.abs(player.x - x);
     }
 
     public void draw(Graphics g) {
@@ -84,24 +90,50 @@ class Fellow {
     }
 
     public void moveFellow(Player player) {
-        x = player.x;
+        x = player.x + difference;
     }
 }
 
 class PlusWall {
-    protected int x1, x2, y;
-
+    protected int width, x, y;
+    protected int x1, x2;
     protected int plusNumber;
 
-    public PlusWall(int x1, int x2, int y) {
-        this.x1 = x1;
-        this.x2 = x2;
+    public PlusWall(int WIDTH, int y) {
+        width = WIDTH / 2;
         this.y = y;
-        plusNumber = (int)(Math.random() * 5) + 1;
+        x = (int)(Math.random() * width) + 1;
+        x1 = x;
+        x2 = x + width;
+        // plusNumber = (int)(Math.random() * 5) + 1;
+        plusNumber = 1;
     }
 
     public void draw(Graphics g) {
         g.setColor(Color.BLUE);
+        g.drawLine(x1, y, x2, y);
+    }
+
+    public void move() {
+        y++;
+    }
+}
+
+class MinusWall {
+    protected int width, x, y, x1, x2;
+    protected int minusNumber;
+
+    public MinusWall(int WIDTH, int y) {
+        width = WIDTH / 2;
+        this.y = y;
+        x = (int)(Math.random() * width) + 1;
+        x1 = x;
+        x2 = x + width;
+        minusNumber = 1;
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(Color.RED);
         g.drawLine(x1, y, x2, y);
     }
 
@@ -118,16 +150,17 @@ class ShootingModel {
     private ArrayList<Enemy> enemies = new ArrayList<>();
     private ArrayList<Fellow> fellows = new ArrayList<>();
     private ArrayList<PlusWall> plusWalls = new ArrayList<>();
-
+    private ArrayList<MinusWall> minusWalls = new ArrayList<>();
     private Player player;
 
     public static final int WIDTH = 400;
     public static final int HEIGHT = 800;
 
     public ShootingModel() {
-        player = new Player(WIDTH / 2 - 10, HEIGHT - 40);
+        player = new Player(WIDTH / 2 - 10, HEIGHT - 100);
         spawnEnemies();
         spawnPlusWall();
+        spawnMinusWall();
     }
 
     // 出現に関するメソッド
@@ -139,13 +172,22 @@ class ShootingModel {
 
     public void spawnPlusWall() {
         for (int i = 0; i < 2; i++) {
-            plusWalls.add(new PlusWall((int) (Math.random() * WIDTH), (int) (Math.random() * WIDTH), (int) (Math.random() * HEIGHT / 2)));
+            plusWalls.add(new PlusWall(WIDTH, (int)(Math.random() * HEIGHT / 2)));
+        }
+    }
+
+    public void spawnMinusWall() {
+        for (int i = 0; i < 2; i++) {
+            minusWalls.add(new MinusWall(WIDTH, (int)(Math.random() * HEIGHT / 2)));
         }
     }
 
     public void spawnFellows(int number) {
         for (int i = 0; i < number; i++) {
-            fellows.add(new Fellow(getPlayer().x + (int)(Math.random() * 201) -100, getPlayer().y + (int)(Math.random() * 201) -100));
+
+            int fellowX = getPlayer().x + (int)(Math.random() * 201) -100;
+            int fellowY = getPlayer().y + (int)(Math.random() * 50) + 1;
+            fellows.add(new Fellow(fellowX, fellowY, getPlayer()));
         }
     }
 
@@ -170,6 +212,10 @@ class ShootingModel {
         return plusWalls;
     }
 
+    public ArrayList<MinusWall> getMinusWall() {
+        return minusWalls;
+    }
+
     // 移動に関するメソッド
     public void moveBullets() {
         bullets.removeIf(bullet -> {
@@ -191,6 +237,15 @@ class ShootingModel {
         });
     }
 
+    public void moveMinusWalls() {
+        minusWalls.removeIf(minusWall -> {
+            minusWall.move();
+            return minusWall.y > HEIGHT;
+        });
+    }
+
+    // 衝突判定に関するメソッド
+
     public void checkCollisions() { // 弾と敵の衝突判定
         Iterator<Enemy> enemyIterator = enemies.iterator();
         while (enemyIterator.hasNext()) {
@@ -206,12 +261,38 @@ class ShootingModel {
         }
     }
 
-    // playerとplusWallの衝突判定
-    public void checkPlusWallCollisions() {
+    public void checkPlusWallCollisions() { // playerとplusWallの衝突判定
         for (PlusWall plusWall : plusWalls) {
-            if (new Rectangle(player.x, player.y, player.size, player.size)
-                    .intersects(new Rectangle(plusWall.x1, plusWall.y, plusWall.x2 - plusWall.x1, 1))) {
+            if (new Rectangle(player.x, player.y, player.size, player.size).intersects(new Rectangle(plusWall.x1, plusWall.y, plusWall.x2 - plusWall.x1, 1))) {
                 spawnFellows(plusWall.plusNumber);
+                break;
+            }
+        }
+    }
+
+    public void checkMinusWallCollisions() { // playerとminusWallの衝突判定
+        for (MinusWall minusWall : minusWalls) {
+            if (new Rectangle(player.x, player.y, player.size, player.size).intersects(new Rectangle(minusWall.x1, minusWall.y, minusWall.x2 - minusWall.x1, 1))) {
+                
+                if (fellows.size() == 0)
+                    break;
+                fellows.remove(minusWall.minusNumber);
+                break;
+            }
+        }
+    }
+
+    public void checkFellowCollisions() { // enemyとfellowの衝突判定
+        Iterator<Enemy> enemyIterator = enemies.iterator();
+        while (enemyIterator.hasNext()) {
+            Enemy enemy = enemyIterator.next();
+            for (Fellow fellow : fellows) {
+                if (new Rectangle(fellow.x, fellow.y, fellow.size, fellow.size)
+                        .intersects(new Rectangle(enemy.x, enemy.y, enemy.size, enemy.size))) {
+                    enemyIterator.remove();
+                    fellows.remove(fellow);
+                    break;
+                }
             }
         }
     }
@@ -247,9 +328,14 @@ class ViewPanel extends JPanel {
             plusWall.draw(g);
         }
 
+        for (MinusWall minusWall : model.getMinusWall()) {
+            minusWall.draw(g);
+        }
+
         for (Fellow fellow : model.getFellows()) {
             fellow.draw(g);
         }
+
     }
 }
 
@@ -274,18 +360,31 @@ class Controller implements KeyListener, ActionListener {
         model.moveBullets();
         model.updateEnemies();
         model.movePlusWalls();
+        model.moveMinusWalls();
+
         model.checkCollisions();
         model.checkPlusWallCollisions();
+        model.checkFellowCollisions();
+        model.checkMinusWallCollisions();
+
         view.repaint();
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
         Player player = model.getPlayer();
+        ArrayList<Fellow> fellows = model.getFellows();
+
         if (e.getKeyCode() == KeyEvent.VK_LEFT && player.x > 0) {
-            player.x -= 10;
+            player.x -= 5;
+            for (Fellow fellow : fellows) {
+                fellow.moveFellow(player);
+            }
         } else if (e.getKeyCode() == KeyEvent.VK_RIGHT && player.x < ShootingModel.WIDTH - player.size) {
-            player.x += 10;
+            player.x += 5;
+            for (Fellow fellow : fellows) {
+                fellow.moveFellow(player);
+            }
         } else if (e.getKeyCode() == KeyEvent.VK_SPACE) {
             model.getBullets().add(new Bullet(player.x + player.size / 2 - 2, player.y));
         }
